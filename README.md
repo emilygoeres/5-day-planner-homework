abbb6f7c3f5cd27510a706e561adc4434d081e59
Emily Goeres <goeres123@hotmail.com>
2020-09-17 10:51:49 -0600

    Updated read me page, changed colors on CSS, added local storage equation

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # 5-day-planner-homework
 -Added an html and css folders to the 5 day planner.
 -Copied code from HTML and CSS over to this VS
--HTML Page I added a table with 9 rows to represent each hour in the day
--Added each hour to each row
--Added the local storage to the html page to save the comments for each hour
+HTML
+-Added a table called myPlanner, in the table I did the following:
+    -Added a row that includes : time, text area so you can type what you need to do and a save button that will save the info to your local storage
+    -I added 9 of these rows to represent each hour between 9am-5pm
+    -Added the link to the script.js page
+    -In each text area, we added an id of the current hour but in military time
+
+Style.css
+-Added background color to the body to gray
+-Changed the title color to crimson
+-Changed the save button colors to crimson
+-Made all of the font for the comments, time and save buttons to be white
+-The change of colors for current hour, past hour and future hour, I changed to past lime green, current, orange, future hot pink
+
+Script.js
+-AAdded the date and time underneath title
+-Created the save button to show when its been clicked and also made it so the comments in the textarea are saved to local storage even when the page refreshes
+-function makes it so your info will save on the website even when your refresh it
+-program runs through all of the hours and tells the program what color the hour needs to be, we did this by utilizing military time, so the txt id whatever time, we gave the variable i and created an if else statement for what happenes if it passes the current hour, if it is the current hour or if the time is in the future
+
+
--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-            var d = new Date();
-            document.getElementById("currentDay").innerHTML = d;
+// Added date and time
+var d = new Date();
+document.getElementById("currentDay").innerHTML = d;
 
-            // added the local storage 
-        // if (typeof (Storage) !== "undefined") {
-            // localStorage.setItem("textarea", "comments");
-            // document.getElementById("result").innerHTML = localStorage.getItem("textarea");
-        // } else {
-            // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
-        // }
+// added the local storage 
+// if (typeof (Storage) !== "undefined") {
+// localStorage.setItem("textarea", "comments");
+// document.getElementById("result").innerHTML = localStorage.getItem("textarea");
+// } else {
+// document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+// }
+// Created the save button to show when its been clicked and also made it so the comments in the textarea are saved to local storage even when the page refreshes.
+$(".saveBtn").on("click", function () {
+    var id = $(this).attr("id")
+    var time = id.split("-")[1]
+    var i = $("#txt-" + time).val()
+    console.log(id, time, "txt-" + time, i)
+    localStorage.setItem(time, i)
+})
+// program runs through all of the hours and tells the program what color the hour needs to be, we did this by utilizing military time
+var currentHour = moment().hour()
+for (var i = 9; i <= 17; i++) {
+    var planner = localStorage.getItem(i)
+    $("#txt-" + i).val(planner)
+    if (currentHour < i) {
+        $("#txt-" + i).addClass("future")
+    } else if (currentHour > i) {
+        $("#txt-" + i).addClass("past")
 
-        $(".saveBtn").on("click",function(){
-            var id=$(this).attr("id")
-            var time= id.split("-")[1]
-            var i=$("#txt-"+time).val()
-            console.log(id,time,"txt-"+time,i)
-            localStorage.setItem(time,i)
-        })
-        for (var i=9; i<=17; i++) {
-            var planner = localStorage.getItem(i)
-            $("#txt-"+i).val(planner)
-        }
+    } else {
+        $("#txt-" + i).addClass("present")
+
+    }
+}
