357f62b9cd7f8671e717c8110eb3625bba0cb3cf
Emily Goeres <goeres123@hotmail.com>
2020-09-16 18:42:33 -0600

    added save buttons, made it so info is saved in local storage

---
+++ script.js
@@ -0,0 +1,18 @@
+            var d = new Date();
+            document.getElementById("currentDay").innerHTML = d;
+
+            // added the local storage 
+        // if (typeof (Storage) !== "undefined") {
+            // localStorage.setItem("textarea", "comments");
+            // document.getElementById("result").innerHTML = localStorage.getItem("textarea");
+        // } else {
+            // document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+        // }
+
+        $(".saveBtn").on("click",function(){
+            var id=$(this).attr("id")
+            var time= id.split("-")[1]
+            var i=$("#txt-"+time).val()
+            console.log(id,time,"txt-"+time,i)
+            localStorage.setItem(time,i)
+        })
